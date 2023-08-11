ad77f107b37715b1caff6f6821b3c8682e6cd423
Obed Paz <lesterpaz99@gmail.com>
2023-08-10 22:31:02 -0600

    Test the libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import { marked } from 'marked'
+import DOMPurify from 'dompurify';
+import parse from 'html-react-parser';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const markedContent = marked.parse('# Hello World');
+  const purifiedContent = DOMPurify.sanitize(markedContent);
+  const jsxContent = parse(purifiedContent);
 
   return (
     <>
+    {jsxContent}
     </>
   )
 }
